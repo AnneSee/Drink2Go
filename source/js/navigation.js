f77786d77cedef6fc5196ca227aab0e69d45ea95
Anna Semenova <a.semenova2018@gmail.com>
2023-05-21 20:38:35 +0700

    Add some code

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,33 +1,20 @@
-const nav = document.querySelector('.main-header__site-nav');
-const navToggle = document.querySelector('.site-nav__toggle');
+document.addEventListener('DOMContentLoaded', () => {
+  const noJsMenu = document.querySelector('.main-header__site-nav');
+  const noJsToggle = document.querySelector('.main-header__toggle');
 
-// Функция, переключающая состояние меню по клику с открытого на закрытое
-navToggle.onclick = function() {
-  // Закрывает меню
-  nav.classList.toggle('main-header__site-nav--closed');
-  // Открывает меню
-  nav.classList.toggle('main-header__site-nav--opened');
-}
+  // Ищет и выключает класс
+  noJsMenu.classList.remove('.main-header__site-nav--nojs');
+  noJsToggle.classList.remove('.main-header__toggle--nojs');
+})
 
-// // Ищет БЭМ-класс меню сайта main-header__site-nav
-// const nav = document.querySelector('.main-header__site-nav');
-// // Ищет БЭМ-класс, содержащий в себе кнопку меню site-nav__toggle
-// const navToggle = document.querySelector('.site-nav__toggle');
-// // Ищет БЭМ-класс без js меню main-header__site-nav--nojs
-// const noJsMenu = document.querySelector('.main-header__site-nav--nojs');
-// // Ищет класс для изображения кнопки menu-toggle
-// const menuToggle = document.querySelector('.menu-toggle');
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('.site-nav');
+  const toggleButton = document.querySelector('.menu-toggle');
 
-// // Скрывает main-header__site-nav--nojs
-// navToggle.classList.remove('.main-header__site-nav--nojs')
+  function toggleMenuOpen() {
+    header.classList.toggle('.main-header__site-nav--open');
+    toggleButton.classList.toggle('.menu-toggle--open');
+  }
 
-// // Функция, переключающая состояние меню по клику с открытого на закрытое
-// function navToggleClosed() {
-//   // Вызывает стиль - Закрывает меню
-//   nav.classList.toggle('main-header__site-nav--closed');
-// }
-
-// // Добавляем обработчик события - клик на закрыть меню
-// nav.addEventListener('click', navToggleClosed);
-
-// function
+  toggleButton.addEventListener('click', toggleMenuOpen)
+})
--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,4 @@
 /* в этот файл добавляет скрипты*/
+import './navigation'
+import './swiper.js'
+import './map.js'
